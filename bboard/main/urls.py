cee66daaba126ef493f6690094ca43a46e0f5c7c
needmoredoggos <donckou@gmail.com>
2022-12-03 05:12:51 +0300

    Изменил стилистику объявления path

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -15,13 +15,49 @@ from .views import (
 app_name = 'main'
 
 urlpatterns = [
-    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
-    path('account/register', RegisterUserView.as_view(), name='register'),
-    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
-    path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change.html'),
-    path('accounts/change/', ChangeUserInfoView.as_view(), name='profile_change'),
-    path('accounts/profile/', profile, name='profile'),
-    path('accounts/login/', BBLoginView.as_view(), name='login'),
-    path('<str:page>/', other_page, name='other'),
-    path('', index, name='index'),
+    path(
+        'accounts/register/done/',
+        RegisterDoneView.as_view(),
+        name='register_done'
+    ),
+    path(
+        'account/register',
+        RegisterUserView.as_view(),
+        name='register'
+    ),
+    path(
+        'accounts/logout/',
+        BBLogoutView.as_view(),
+        name='logout'
+    ),
+    path(
+        'accounts/password/change/',
+        BBPasswordChangeView.as_view(),
+        name='password_change.html'
+    ),
+    path(
+        'accounts/change/',
+        ChangeUserInfoView.as_view(),
+        name='profile_change'
+    ),
+    path(
+        'accounts/profile/',
+        profile,
+        name='profile'
+    ),
+    path(
+        'accounts/login/',
+        BBLoginView.as_view(),
+        name='login'
+    ),
+    path(
+        '<str:page>/',
+        other_page,
+        name='other'
+    ),
+    path(
+        '',
+        index,
+        name='index'
+    ),
 ]
