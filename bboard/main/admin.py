2ac9842c08c74f03b4b1d98abd4a24b512706ae6
needmoredoggos <donckou@gmail.com>
2022-12-05 20:31:35 +0300

    Функции send_activation_notifications добавлена

--- bboard/main/admin.py
+++ bboard/main/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+import datetime
+
 from .models import AdvUser
+from .utilities import send_activation_notification
+
 
 # Register your models here.
-admin.site.register(AdvUser)
+def send_activation_notifications(modeladmin, request, queryset):
+    for rec in queryset:
+        if not rec.is_activated:
+            send_activation_notification(rec)
+    modeladmin.message_user(request, 'Письма с требования отправлены')
+    send_activation_notifications.short_description = 'Отправка писем с требованием активации'
+
+
